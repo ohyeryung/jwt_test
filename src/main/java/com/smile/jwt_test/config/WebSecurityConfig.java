e0eefd70c7dbb514b37cb32f529a8dafb7e043a7
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-28 16:54:46 +0900

    :wrench: [Chore] WebSecurityConfig 설정

---
+++ src/main/java/com/smile/jwt_test/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.smile.jwt_test.config;
+
+import com.smile.jwt_test.auth.JwtAuthenticationFilter;
+import com.smile.jwt_test.auth.token.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.security.SecureRandom;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecureRandom secureRandom() {
+
+        return new SecureRandom();
+    }
+
+    private final TokenProvider tokenProvider;
+
+    public WebSecurityConfig(TokenProvider tokenProvider) {
+
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+
+        return (web) -> web.ignoring().requestMatchers("/h2-console/**"
+                , "/favicon.ico"
+                , "/error");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((AbstractHttpConfigurer::disable))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/users/**", "/h2-console/**", "/swagger-ui/**").permitAll()
+
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
